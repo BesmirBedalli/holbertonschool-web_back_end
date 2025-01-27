8f4fb9ee1a633ef0bfd95985c3cd923f69ff87fe
BesmirBedalli <bbedalli1987>
2025-01-27 17:12:54 +0100

    changes

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(listOfStudents) {
-	return listOfStudents.reduce((sum, student) => sum + student.id, 0);
+  return listOfStudents.reduce((sum, student) => sum + student.id, 0);
 }

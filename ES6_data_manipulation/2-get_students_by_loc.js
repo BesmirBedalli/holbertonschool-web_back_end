6c8647f84999b35a1e31ed308385678379fb8cd2
BesmirBedalli <bbedalli1987>
2025-01-27 17:09:26 +0100

    changes

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
 export default function getStudentsByLocation(listOfStudent, city) {
-	return listOfStudent.filter((cities) => cities.location === city);
+  return listOfStudent.filter((cities) => cities.location === city);
 }

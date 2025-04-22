940056b2f9f006ac66f623995191e3b3917cc8f5
BesmirBedalli <bbedalli1987@gmail.com>
2025-04-22 17:13:52 +0200

    changes

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,22 +1,21 @@
 export default class HolbertonCourse {
-
   constructor(name, length, students) {
     if (typeof name === 'string') {
       this._name = name;
     } else {
-      throw new Error('Name must be a string')
+      throw new Error('Name must be a string');
     }
 
     if (typeof length === 'number') {
       this._length = length;
     } else {
-      throw new Error('Length must be a number')
+      throw new Error('Length must be a number');
     }
 
     if (Array.isArray(students)) {
       this._students = students;
     } else {
-      throw new Error('Students must be an array of strings')
+      throw new Error('Students must be an array of strings');
       }
   }
 
@@ -36,25 +35,25 @@ export default class HolbertonCourse {
     if (typeof name === 'string') {
         return this._name = name;
     } else {
-        throw new Error('Name must be a string')
+        throw new Error('Name must be a string');
     }
 
-    }
+  }
 
   set length(length) {
     if (typeof length === 'number') {
         return this._length = length;
     } else {
-        throw new Error('Length must be a number')
+        throw new Error('Length must be a number');
     }
-    }
+  }
 
   set students(students) {
     if (Array.isArray(students)) {
         return this._students = students;
     } else {
-        throw new Error('Students must be an array of strings')
-    }
+        throw new Error('Students must be an array of strings');
+  }
 
     }
 }
